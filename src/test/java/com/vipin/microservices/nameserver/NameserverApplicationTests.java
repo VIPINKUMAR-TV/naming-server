c1b47ffef83b328a62a18422635ff9ba36356215
Vipinkumar <tvkumarvipin@gmail.com>
2025-04-04 17:37:35 +0530

    naming server

---
+++ src/test/java/com/vipin/microservices/nameserver/NameserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipin.microservices.nameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
